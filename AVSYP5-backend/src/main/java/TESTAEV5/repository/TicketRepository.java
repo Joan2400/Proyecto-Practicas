06ddb6bffbf7a51b6f16a473b53cca138d790ee9
Joan2400 <56951990+Joan2400@users.noreply.github.com>
2023-03-31 11:12:22 +0200

    Delete AVSYP5-backend directory

--- AVSYP5-backend/src/main/java/TESTAEV5/repository/TicketRepository.java
+++
@@ -1,8 +0,0 @@
-package TESTAEV5.repository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-import TESTAEV5.model.Ticket;
-@Repository
-public interface TicketRepository extends CrudRepository<Ticket, Integer>  {
-}
